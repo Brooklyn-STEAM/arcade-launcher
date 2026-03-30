32695d6720e119073172823352ae5400fc044a68
Justin <justin@justinforlenza.dev>
2026-03-29 20:26:48 -0400

    migrated to tauri

--- src/mainview/App.tsx
+++
@@ -1,3 +0,0 @@
-export default function App() {
-  return <h1>Arcade Launcher</h1>
-}
--- src/mainview/main.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from 'solid-js/web'
-import './style.css'
-import App from './App'
-
-render(() => <App />, document.getElementById('app')!)
